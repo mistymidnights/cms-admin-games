0f1b3280d773d7f05f2fe9d790bc7ec3e7e74ab8
pedroleni <pedroleridanieto@hotmail.com>
2022-09-16 17:22:03 +0200

    solve

--- src/components/NewPost.element.jsx
+++ src/components/NewPost.element.jsx
@@ -2,12 +2,11 @@ import styled from "@emotion/styled";
 
 export const NewPostHero = styled.div`
   width: 100%;
-  /* height: 100vh; */
   display: flex;
   justify-content: center;
   align-items: center;
   padding-bottom: 100px;
-  padding-top: 100px;
+  padding-top: 20px;
 `;
 
 export const FormNewPost = styled.form`
